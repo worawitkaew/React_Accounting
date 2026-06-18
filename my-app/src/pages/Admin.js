55699ddb1bc9e6cdfaa3137ddaa9b9936197fdab
worawitkaew <mon2350@hotmail.com>
2026-06-18 10:38:03 +0700

    ผ่าน React ขั้น 1

--- my-app/src/pages/Admin.js
+++ my-app/src/pages/Admin.js
@@ -1,10 +1,14 @@
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 function Admin() {
 
   const navigate = useNavigate();
 
   const role = localStorage.getItem("role");
+
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
 
   if (role !== "admin") {
     return (
@@ -19,19 +23,74 @@ function Admin() {
   }
 
   const handleLogout = () => {
-
     localStorage.removeItem("role");
-
     navigate("/");
+  };
+
+  const handleAddProduct = () => {
+
+    fetch("http://localhost:8000/products", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        name: name,
+        price: Number(price)
+      })
+    })
+      .then((response) => response.json())
+      .then((data) => {
+
+        alert("เพิ่มสินค้าสำเร็จ");
+
+        setName("");
+        setPrice("");
+
+        console.log(data);
+
+      });
+
   };
 
   return (
     <div>
+
       <h1>Admin Panel</h1>
+
+      <h2>เพิ่มสินค้า</h2>
+
+      <input
+        type="text"
+        placeholder="ชื่อสินค้า"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+
+      <br />
+      <br />
+
+      <input
+        type="number"
+        placeholder="ราคา"
+        value={price}
+        onChange={(e) => setPrice(e.target.value)}
+      />
+
+      <br />
+      <br />
+
+      <button onClick={handleAddProduct}>
+        เพิ่มสินค้า
+      </button>
+
+      <br />
+      <br />
 
       <button onClick={handleLogout}>
         Logout
       </button>
+
     </div>
   );
 }
--- my-app/src/pages/Product.js
+++ my-app/src/pages/Product.js
@@ -1,34 +1,38 @@
+import { useEffect, useState } from "react";
+
 function Product() {
 
-  const products = [
-    {
-      id: 1,
-      name: "น้ำสปอนเซอร์",
-      price: 15
-    },
-    {
-      id: 2,
-      name: "น้ำเปล่า",
-      price: 10
-    },
-    {
-      id: 3,
-      name: "มด",
-      price: 300
-    }
-  ];
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+
+    fetch("http://localhost:8000/products")
+      .then((response) => response.json())
+      .then((data) => {
+        setProducts(data);
+      });
+
+  }, []);
 
   return (
     <div>
+
       <h1>สินค้าในร้านดาวตก</h1>
 
       {products.map((item) => (
+
         <div key={item.id}>
+
           <h3>{item.name}</h3>
+
           <p>ราคา {item.price} บาท</p>
+
           <hr />
+
         </div>
+
       ))}
+
     </div>
   );
 }
